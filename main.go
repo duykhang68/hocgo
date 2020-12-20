5ac6563aa38a4d6bf7e5dcd9b370432b52b609bc
duykhang68 <duykhang6898@gmail.com>
2020-12-20 18:58:15 +0700

    commit code golang

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main (){
+func main() {
 	fmt.Println("Hello Golang")
+	fmt.Println(Sum(3)) // o + 1 + 2
+}
+func Sum(number int) int {
+	Sum := 0
+	for i := 0; i < number; i++ {
+		Sum += i
+
+	}
+	return Sum
 }
